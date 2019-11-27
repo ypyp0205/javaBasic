9b02bd40e1a0ce93162068c4bfb8afe0877b23cc
yp0205 <gogo5451@naver.com>
2019-11-27 23:51:04 +0900

    2019.11.27 집에서 조금 본것

--- src/d_array/TSSSSSSSSS.java
+++ src/d_array/TSSSSSSSSS.java
@@ -2,14 +2,15 @@ package d_array;
 
 import java.util.Arrays;
 
+
 public class TSSSSSSSSS {
 
 	public static void main(String[] args) {
 
 
 			
-		
-		/*int[] numbers = new int[10];
+		/*
+		int[] numbers = new int[10];
 
 		for (int i = 0; i < numbers.length; i++) {
 			numbers[i] = (int) (Math.random() * 100) + 1;
@@ -27,10 +28,10 @@ public class TSSSSSSSSS {
 			}
 
 		System.out.println("최대값 : " + max);
-		System.out.println("최소값 : " + min);*/
+		System.out.println("최소값 : " + min);
+		*/
 		
-		
-		int[] abc = new int[10];
+/*		int[] abc = new int[10];
 
 		for (int i = 0; i < abc.length; i++) {
 			abc[i] = (int) (Math.random()*100)+1;
@@ -47,9 +48,42 @@ public class TSSSSSSSSS {
 				min = abc[i];
 			}
 		System.out.println("최대값 : " + max);
-		System.out.println("최소값 : " + min);
+		System.out.println("최소값 : " + min);*/
 		
 		
+		
+		/*int[] ko = new int[10];
+		
+		for(int i = 0; i < ko.length; i++){
+			ko[i] = (int)(Math.random()*100)+1;
+		}
+		int mx = ko[0];
+		int mn = ko[0];
+		
+		for(int i = 0; i < ko.length; i++)
+		if( mx < ko[i]){
+			mx = ko[i]; 
+		}	
+		else if( mn > ko[i]){
+			     mn = ko[i];
+		}
+		System.out.println("최대값은" + mx + "최소값은" + mn);*/
+		
+		
+		
+		int[] ko = new int[10];
+			
+		for(int i = 0; i < 500; i++){
+			int bo = (int)(Math.random()*10)+1;
+			ko[bo-1]++;
+		}
+			System.out.println(Arrays.toString(ko));
+			
+		
+				
+		
+		
+	
 		
 		
 		
