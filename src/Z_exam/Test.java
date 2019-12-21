190d461bf8a988b9dc25d5f0c4b72949c0e62edf
ypyp0205 <gogo5451@naver.com>
2019-12-21 21:45:56 +0900

    1

--- src/Z_exam/Test.java
+++ src/Z_exam/Test.java
@@ -1,2 +1,28 @@
+package Z_exam;
 
-				
+import java.util.Date;
+
+
+public class Test {
+	
+public static void main(String[] args) {
+			
+		
+		
+		
+		
+		Date today = new Date();
+		
+		System.out.println(today);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}}
