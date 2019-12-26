17eb27ae36dde2c9ebc5c6336c20a3449a1a2825
yp0205 <gogo5451@naver.com>
2019-12-26 20:51:43 +0900

    1

--- src/Z_exam/Test.java
+++ src/Z_exam/Test.java
@@ -1,18 +1,47 @@
 package Z_exam;
 
+import java.awt.List;
+import java.util.ArrayList;
 import java.util.Date;
+
+import d_array.Array;
 
 
 public class Test {
 	
 public static void main(String[] args) {
-			
-		int a = 0;
-		for (int i = 1; i < 11; i++) {
-			a += i;
-			
-		}
+
+
+	ArrayList<Integer> intList = new ArrayList<Integer>();
+	intList.add(10);
+	intList.add(15);
+	intList.add(20);
+	intList.add(25);
+	intList.add(30);
+	intList.add(35);
+	intList.add(40);
+	
+	//리스트에 담긴 값의 간격의 평균
+	
+	
+	int sum = 0;
+	int avg = 0;
+	for (int i = 0; i < intList.size()-1; i++) {
+	
+		int a =	intList.get(i + 1) - intList.get(i);
+		sum += a;
+		avg = sum / (intList.size() - 1);
+	}
+	
+	System.out.println(avg);
 		
-		
-		
-	}}
+	
+
+	
+	
+
+	
+	
+	
+	}
+}
